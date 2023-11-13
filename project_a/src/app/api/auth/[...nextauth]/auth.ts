112395ac075156f36f105e83cb3219e0aa901091
Teerawat chuaphanngam <99793968+phukaokkSIIT@users.noreply.github.com>
2023-11-13 14:35:28 +0700

    fix import authOptinos and export authOptions

--- project_a/src/app/api/auth/[...nextauth]/auth.ts
+++ project_a/src/app/api/auth/[...nextauth]/auth.ts
@@ -1,20 +1,21 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_SECRET_ID as string,
+      clientId: process.env.GITHUB_CLIENT_ID ?? "" ,
+      clientSecret: process.env.GITHUB_SECRET_ID ?? "" ,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "" ,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "" ,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET as string,
+  secret: process.env.NEXTAUTH_SECRET ?? "",
 };
 
 const handler = NextAuth(authOptions);
